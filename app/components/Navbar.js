a7d6698cd6bbad7496209b496e62429cb8c6f6b0
kritigaba710 <kritigaba31@gmail.com>
2025-04-19 06:42:29 -0700

    final commit

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between p-5 h-20 font-bold text-2xl text-[#95a9ce] bg-slate-950'>
+    <div className='flex items-center justify-between p-5 h-20 font-bold text-sm md:text-2xl text-[#95a9ce] bg-slate-950'>
       <div>Visualizer</div> 
       <div>Manage your Finance here</div>
     </div>
